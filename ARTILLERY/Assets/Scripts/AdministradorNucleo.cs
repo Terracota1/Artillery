3f9ffeabec30a6382b6daa50933ea51b5ddc66ad
Terracota1 <gianlucadelarosa@gmail.com>
2023-09-03 20:52:20 -0600

    Audio
    Se cambio el audio y los efectos.

--- ARTILLERY/Assets/Scripts/AdministradorNucleo.cs
+++ ARTILLERY/Assets/Scripts/AdministradorNucleo.cs
@@ -8,12 +8,12 @@ public class AdministradorNucleo : MonoBehaviour
     public UnityEvent GameWon;
     public GameObject Menu_Fin_Nivel;
 
-    private void OnTriggereEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Explosion")
         {
-            GameWon.Invoke();
             Destroy(this.gameObject);
+            GameWon.Invoke();
             //Menu_Fin_Nivel.SetActive(true);
         }
     }
